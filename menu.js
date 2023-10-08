488da3b618cbc7b5854e55636504cfa57b2a254f
Rodrigo Moreira <rodrigojs20@gmail.com>
2023-10-08 15:01:12 -0300

    Adicionado Menu Mobile

--- menu.js
+++ menu.js
@@ -1,17 +1,8 @@
-const menu = document.querySelector("#btnMenu");
+const menuMobile = document.querySelector("#btnMenu");
 
-function toggleMenu(event) {
-  if (event.type === "touchstart") event.preventDefault();
+function toogleMenu() {
   const nav = document.querySelector("#nav");
   nav.classList.toggle("ativo");
-  const ativo = nav.classList.contains("ativo");
-  event.currentTarget.setAttribute("aria-expanded", ativo);
-  if (ativo) {
-    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
-  } else {
-    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
-  }
 }
 
-menu.addEventListener("click", toggleMenu);
-menu.addEventListener("touchstart", toggleMenu);
+menuMobile.addEventListener("click", toogleMenu);
